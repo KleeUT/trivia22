8b98bf8c8e75cf1e9e3e8e74d72fa23e88feacba
Klee Thomas <klee.ut@gmail.com>
2022-11-09 21:10:06 +1100

    :alembic: :loud_sound:

--- src/routes/api/currentQuestion/+server.ts
+++ src/routes/api/currentQuestion/+server.ts
@@ -9,9 +9,10 @@ export async function GET({request, platform}:RequestEvent) : Promise<Response> 
 		if(!v){
 			return new Response(JSON.stringify({err: {message: "No current question"}}),{status:404})
 		}
-		const question = JSON.parse(v) as CurrentQuestion
+		const question = JSON.parse(v) as CurrentQuestion;
 		return new Response(JSON.stringify({data:{currentQuestion: question}}), { status: 200 });
 	}catch(e){
-		return new Response(JSON.stringify({err: e}), {status:500})
+		const err = e as Error;
+		return new Response(JSON.stringify({err: {message:err.message, stack:err.stack}}), {status:500})
 	}
 };
